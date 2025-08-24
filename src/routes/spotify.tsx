b4846738e0c5fa2cee0d06ca804044febf7d87e2
Satyam Mandal <satyam.mandal.77@gmail.com>
2025-08-24 16:05:32 +0530

    Theme Setup

---
+++ src/routes/spotify.tsx
@@ -0,0 +1,12 @@
+import { JSXElement } from 'solid-js';
+
+function Spotify(): JSXElement {
+  return (
+    <div>
+      <h1>Spotify</h1>
+      <p>Spotify is a music streaming service that allows users to listen to music on the go.</p>
+    </div>
+  );
+}
+
+export default Spotify;
